544f15966d34db56a18c827b10e56b803ecaaded
Eddie Chu <contact@eddiechu.me>
2018-12-29 23:09:37 -0800

    updated css file, add nav-bar button functionality

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 
 //cryptocurrency api
 const api_key1 ="960e428fdca399a09e41196327f5766b1f76aa979eec604f31318a4e0ac3f032";
-//news api
-const api_key2 = "3d4f6056ea124d38aff81d043310b2f2";
+//contains an array of responseJson data from API endpoint AllCoins
+let dataArray;
+//counter for All Coins page's index in the array dataArray
+let pageCounter = 0;
 
 //formats parameters for urls
 function formatQueryParams(params) {
@@ -34,8 +36,7 @@ function reloadPage() {
 
 //event for clicking "Top 10 Coins"
 function registerNavbtnI() {
-    $(".nav-btn1").on("click",function(event){
-        event.preventDefault();
+    $(".nav-btn1").on("click",function(event) {
         $(this).prop("disabled", true);
         $(".landing").empty();
         generateTopTenLayout();
@@ -43,8 +44,20 @@ function registerNavbtnI() {
     })
 }
 
+//event for clicking "All Coins"
+function registerNavbtnII() {
+    $(".nav-btn2").on("click", function(event) {
+        $(this).prop("disabled", true);
+        pageCounter = 0;
+        $(".landing").empty();
+        generateAllCoinsLayout();
+        generateAllCoins();
+    })
+
+}
+
 //event for clicking "About"
-function generateAbout() {
+function registerAbout() {
     $(".about-btn").on("click", function (event) {
         $(".landing").empty();
         $(".landing").append(`<div class="about-page">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -57,16 +70,28 @@ function generateAbout() {
     });
 }
 
-//generates html elements for generateTopTen() to append future li's
+//generates html elements for generateTopTen() to append future td's
 function generateTopTenLayout() {
-    $(".landing").hide().fadeIn().append(`
+    $(".landing").hide().fadeIn().html(`
         <section>
-            <h2>Toplist by 24 Hour Volume Subscriptions</h2>
+            <h2 class="hidden">Top Ten List by 24 Hour Volume Subscriptions</h2>
                 <table class="top-currencies">
                     <tr class="main-table">
                         <th>Name</th>
                         <th>Price</th>
                         <th>High/Low</th>
+                    </tr>
+                </table>
+        </section>`)
+}
+
+//generates html elements for generateAllCoins() to append future td's
+function generateAllCoinsLayout() {
+    $(".landing").hide().fadeIn().html(`
+        <section>
+            <h2>All Coins</h2>
+                <table class="all-currencies">
+                    <tr class="main-table">
                     </tr>
                 </table>
         </section>`)
@@ -93,43 +118,97 @@ function generateTopTen() {
             } throw new Error(response.statusText);
         })
         .then(function (responseJson) {
-            for( let i=0; i<responseJson.Data.length; i++) {
-                $(".top-currencies").hide().fadeIn().append(`
+            let tableitems = "";
+            for (let i=0; i<responseJson.Data.length; i++) {
+                tableitems = tableitems.concat(`
                 <tr class="table-info">
                     <td class="cName"><img class="icons" src="${baseImageUrl}${responseJson.Data[i].CoinInfo.ImageUrl}">${responseJson.Data[i].CoinInfo.FullName}</td>
                     <td class="price">${responseJson.Data[i].DISPLAY.USD.PRICE}</td>
                     <td class="volume">${responseJson.Data[i].DISPLAY.USD.HIGHDAY}/${responseJson.Data[i].DISPLAY.USD.LOWDAY}</td>
-                </tr>
-                `);
+                </tr>`);
             }
-      
+            $(".top-currencies").hide().fadeIn().append(tableitems);
+            $("h2.hidden").removeClass("hidden");
+
         })
         .catch(error => console.log("generateTopTen failed"));
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// landing page generates 3 most recent news articles for cryptocurrency 
-// function generateNews(){
-//     const searchUrl = "https://newsapi.org/v2/everything?q=cryptocurrency+blockchain&apiKey=";
-//     const url = searchUrl + api_key2;
-//     return fetch(url)
-//         .then(response => {
-//             if (response.ok){
-//                 return response.json();
-//             } throw new Error(response.statusText);
-//         })
 
-//         .then(function (responseJson){
-//             for(let i=0; i<6;i++){
-//                 $(".news-articles").append(`
-//                     <article class="article"><a class="a-link" href="${responseJson.articles[i].url}"><span>${responseJson.articles[i].title}</span><span>${responseJson.articles[i].source.name}</span><img src="${responseJson.articles[i].urlToImage}"></a></article>
-//                     `)}
-//                 })
-//         .catch(error => console.log("generateNews failed"))
+//fetch endpoint "all coin list"
+function generateAllCoins() {
+    const searchUrl = "https://min-api.cryptocompare.com/data/all/coinlist";
+
+    return fetch(searchUrl)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } throw new Error(response.statusText);
+        })
+        .then(function (responseJson) {
+            dataArray = [];
+            for (let key in responseJson.Data){
+                dataArray.push(responseJson.Data[key]);
+            }
+            console.log("made it to generateQ");
+            generateQuantity(dataArray);
+
+            // for (let i = 0; i < 20; i++) {
+            //      $(".all-currencies").append(`
+            //     <tr class="table-info">
+            //         <td class="cName"><img class="icons" src="${baseImageUrl}${responseJson.Data[i].ImageUrl}">${responseJson.Data[i].FullName}</td>
+            //     </tr>
+            //     `   console.log("made it to loop generateAllCoins");
+            // );
+            // }
+        })
+        .catch(error => console.log("generateAllCoins failed",error));
+
+}
+
+function generateQuantity(arr) {
+    let tableitems2 = "";
+    const baseImageUrl = "https://www.cryptocompare.com/";
+    for (let i=pageCounter; i<pageCounter+20; i++) {
+        tableitems2 = tableitems2.concat(`
+        <tr class="table-info">
+            <td><img class = "icons" src="${baseImageUrl}${arr[i].ImageUrl}">${arr[i].FullName}</td>
+            </tr>`);
+    }
+
+    if (pageCounter === 0) {
+    $(".all-currencies").hide().fadeIn().append(tableitems2);
+    $(".all-currencies").hide().fadeIn().append(`<div><span class="next-load">Next -></span></div>`);
+    } else {
+        $(".all-currencies").hide().fadeIn().append(tableitems2);
+        $(".all-currencies").hide().fadeIn().append(`<div class="index-nav"><span class="previous-load"><- Previous </span><span class="next-load"> Next -></span></div>`);
+    }
+
+    
+}
+
+//register event for next button on All Coins
+function loadNextCoins() {
+    $(".landing").on("click", ".next-load", function(event){
+        $(".all-currencies").empty();
+        pageCounter = pageCounter + 20;
+        generateQuantity(dataArray);
+    })
+}
+
+//register event for previous button on All Coins
+function loadPreviousCoins() {
+    $(".landing").on("click", ".previous-load", function (event) {
+        $(".all-currencies").empty();
+        pageCounter = pageCounter - 20;
+        generateQuantity(dataArray);
+    })
+}
+// function renderObject(item){
+//     let tableitems = "";
+//     $(".all-currencies").concat(`<td>${item.FullName}</td>`)
 
 // }
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //event for submitting a search on landing page
 function registerEvents(){
@@ -183,14 +262,15 @@ function generateResults(search, exchange, currency) {
         })
 
         .then(function (responseJson) {// seperate function?
-            $(".landing").append(`<div class="checktest"></div>`);
-            $(".checktest").append(`<div>Price: ${responseJson.DISPLAY.PRICE}</div><div>`)
-            $(".checktest").append(`<div>Open24: ${responseJson.DISPLAY.OPEN24HOUR}</div>`)
-            $(".checktest").append(`<div>High24: ${responseJson.DISPLAY.HIGH24HOUR}</div>`)
-            $(".checktest").append(`<div>Low24: ${responseJson.DISPLAY.LOW24HOUR}</div>`)
-            $(".checktest").append(`<div>Change24: ${responseJson.DISPLAY.CHANGE24HOUR}</div>`)
-            $(".checktest").append(`<div>ChangePercent: ${responseJson.DISPLAY.CHANGEPCT24HOUR}%</div>`)
-        })
+            $(".landing").append(`<div class="search-results"></div>`);
+            $(".search-results").append(`
+                <div>Price: ${responseJson.DISPLAY.PRICE}</div>
+                <div>Open24: ${responseJson.DISPLAY.OPEN24HOUR}</div>
+                <div>High24: ${responseJson.DISPLAY.HIGH24HOUR}</div>
+                <div>Low24: ${responseJson.DISPLAY.LOW24HOUR}</div>
+                <div>Change24: ${responseJson.DISPLAY.CHANGE24HOUR}</div>
+                <div>ChangePercent: ${responseJson.DISPLAY.CHANGEPCT24HOUR}%</div>
+            `)})
         .catch(error => console.log("1 error happened"));
 }
 
@@ -212,21 +292,20 @@ function generateResultsMedia(search) {
 
         .then(function (responseJson) {
             console.log("123", responseJson.Data[search].FullName)
-            $(".checktest").append(`<div>${responseJson.Data[search].FullName}</div>`);
+            $(".search-results").append(`<div>${responseJson.Data[search].FullName}</div>`);
             
         })
         .catch(error => console.log(error));
 }
 
 function documentReady() {
-    // generateTopTenLayout();
-    // generateTopTen();
-    // generateNews();
     registerEvents();
     registerNavbtnI();
-    generateAbout();
+    registerNavbtnII();
+    loadNextCoins();
+    loadPreviousCoins();
+    registerAbout();
     reloadPage();
-    // test();
 }
 
 $(documentReady);
